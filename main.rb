05897293473494e811f6f7441f30c24ddce73ad0
summerxx <summerxx27@163.com>
2023-01-19 21:31:28 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -39,16 +39,24 @@ class Network
             # 回调data数据
             yield data
         else 
-            puts "请求失败 : Response code = #{res.code}, message = #{res.message}"
+            puts "url = #{url}, 请求失败 : Response code = #{res.code}, message = #{res.message}"
         end
     end
 
-    def Network.post (url) 
+    def Network.get (url, &block) 
         uri = URI.parse(url)
         https = Net::HTTP.new(uri.host,uri.port)
         https.use_ssl = true
         req = Net::HTTP::Get.new(uri.path, initheader = {'Content-Type' =>'application/json'})
         res = https.request(req)
+        if res.code == "200"
+            data = JSON.parse(res.body)
+            # 回调data数据
+            yield data
+        else 
+            yield false
+            puts "url = #{url}, 请求失败 : Response code = #{res.code}, message = #{res.message}"
+        end
     end
 end
 
@@ -61,7 +69,7 @@ book = Spreadsheet::Workbook.new
 sheet1 = book.create_worksheet :name => "summerxx专属表"
 #格式
 sheet1.row(0).height = 28
-for i in 0..17
+for i in 0..19
     sheet1.column(i).width = 20
 end
 
@@ -83,6 +91,8 @@ sheet1.row(0)[13] = "archived"
 sheet1.row(0)[14] = "disabled"
 sheet1.row(0)[15] = "license"
 sheet1.row(0)[16] = "language"
+sheet1.row(0)[17] = "openranksum"
+sheet1.row(0)[18] = "activitysum"
 
 # 参照: https://github.com/Shopify/cli-ui
 username = CLI::UI::Prompt.ask('请输入您要查询的用户名例如 ruby:')
@@ -123,35 +133,65 @@ Network.request(url) { | data |
             Network.request(url) { | data |
                 # 判断 repo 非空
                 if data
-                    puts "full_name = #{data["full_name"]}, created_at = #{data["created_at"]}, i = #{i}}"
-                    sheet1.row(i + 1)[0] = data["full_name"]
-                    sheet1.row(i + 1)[1] = data["html_url"]
-                    sheet1.row(i + 1)[2] = data["description"]
-                    sheet1.row(i + 1)[3] = data["forks_count"]
-                    sheet1.row(i + 1)[4] = data["created_at"]
-                    sheet1.row(i + 1)[5] = data["updated_at"]
-                    sheet1.row(i + 1)[6] = data["pushed_at"]
-                    sheet1.row(i + 1)[7] = data["stargazers_count"]
-                    sheet1.row(i + 1)[8] = data["subscribers_count"]
-                    sheet1.row(i + 1)[9] = data["open_issues_count"]
-                    sheet1.row(i + 1)[10] = data["fork"]
-                    sheet1.row(i + 1)[11] = data["has_wiki"]
-                    sheet1.row(i + 1)[12] = data["has_pages"]
-                    sheet1.row(i + 1)[13] = data["archived"]
-                    sheet1.row(i + 1)[14] = data["disabled"]
-                    if data["license"] 
-                        license = data["license"]
-                        name = license["name"]
-                        sheet1.row(i + 1)[15] = name
-                    end
-                    sheet1.row(i + 1)[16] = data["language"]
 
-                        if i == names.count - 1
-                            puts "excel 已经导出成功, 请查看桌面"
-                            #在指定路径下面创建表格，并写book对象
-                            book.write "/Users/summerxx/Desktop/rubyExcel/summerxx专属表.xls"
-                            DingDing.send_markdown("excel导出完成", "请查看桌面summerxx, 表导出成功啦")
+                    openrankurl = "https://oss.x-lab.info/open_digger/github/#{data["full_name"]}/openrank.json"
+                    activityurl = "https://oss.x-lab.info/open_digger/github/#{data["full_name"]}/activity.json"
+
+                    Network.get(openrankurl) { | v |
+                        openrank_sum = 0.0
+                        activity_sum = 0.0
+                        if v 
+                            values = Hash.new
+                            values = v
+                            values.each_value { | value |
+                                openrank_sum = openrank_sum + value
+                            }
                         end
+
+                        Network.get(activityurl) { | v |
+                            sum = 0.0
+                            if v 
+                                values = Hash.new
+                                values = v
+                                values.each_value { | value |
+                                    activity_sum = activity_sum + value
+                                }
+                            end
+
+                            puts "full_name = #{data["full_name"]}, created_at = #{data["created_at"]}, i = #{i}}"
+                            sheet1.row(i + 1)[0] = data["full_name"]
+                            sheet1.row(i + 1)[1] = data["html_url"]
+                            sheet1.row(i + 1)[2] = data["description"]
+                            sheet1.row(i + 1)[3] = data["forks_count"]
+                            sheet1.row(i + 1)[4] = data["created_at"]
+                            sheet1.row(i + 1)[5] = data["updated_at"]
+                            sheet1.row(i + 1)[6] = data["pushed_at"]
+                            sheet1.row(i + 1)[7] = data["stargazers_count"]
+                            sheet1.row(i + 1)[8] = data["subscribers_count"]
+                            sheet1.row(i + 1)[9] = data["open_issues_count"]
+                            sheet1.row(i + 1)[10] = data["fork"]
+                            sheet1.row(i + 1)[11] = data["has_wiki"]
+                            sheet1.row(i + 1)[12] = data["has_pages"]
+                            sheet1.row(i + 1)[13] = data["archived"]
+                            sheet1.row(i + 1)[14] = data["disabled"]
+                            sheet1.row(i + 1)[16] = data["language"]
+                            sheet1.row(i + 1)[17] = openrank_sum
+                            sheet1.row(i + 1)[18] = activity_sum
+
+                            if data["license"] 
+                                license = data["license"]
+                                name = license["name"]
+                                sheet1.row(i + 1)[15] = name
+                            end
+
+                            if i == names.count - 1
+                                puts "excel 已经导出成功, 请查看桌面"
+                                #在指定路径下面创建表格，并写book对象
+                                book.write "/Users/summerxx/Desktop/rubyExcel/summerxx专属表.xls"
+                                DingDing.send_markdown("excel导出完成", "请查看桌面summerxx, #{username}表导出成功啦")
+                            end
+                        }
+                    }
                 end
             }
         end
